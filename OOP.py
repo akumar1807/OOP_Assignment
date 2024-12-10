de8e5f2a00d708d54dccde191192b4685927a395
AyushKumar <ayush18.kumar@gmail.com>
2024-12-10 10:55:56 +0530

    Task 2 completed

--- OOP.py
+++ OOP.py
@@ -4,16 +4,25 @@ class Car:
     def __init__(self, make, model, price):
         self.make = make
         self.model = model
-        self.price = price
+        self.__price = price
     
     def apply_discount(self, discount):
         self.price -= (discount*self.price)/100
         pass
 
     def get_info(self):
-        info = (self.make, self.model, self.price)
+        info = (self.make, self.model, self.get_price())
         return info
+    
+    def get_price(self):
+        return self.__price
+    
+    def set_price(self, p):
+        self.__price = p
     
 #_main_
 task1 = Car("Hyundai", "Verna", 10000)
 print(task1.get_info())
+print(task1.get_price())
+task1.set_price(20000)
+print(task1.get_price())
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## Task 1  
 Created a class **Car** with attributes and two methods. Created a main program to test one of the methods  
 
+## Task 2  
+Made the **price** attribute private and made a getter and setter method to modify the price. Demonstrated how they worked via a main program
